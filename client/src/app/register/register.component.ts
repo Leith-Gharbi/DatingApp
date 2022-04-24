fa617eab9d289b698a9dbcedf4c76d19b9159361
Gharbi <leithgharbi@yahoo.fr>
2022-04-24 11:28:10 +0200

    Adding a toast service for notifications

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -10,7 +11,10 @@ import { AccountService } from '../_services/account.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor(private accountService: AccountService) {}
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {}
 
@@ -21,7 +25,10 @@ export class RegisterComponent implements OnInit {
         console.log(response);
         this.cancel();
       },
-      (error) => console.log(error)
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.error);
+      }
     );
   }
   cancel() {

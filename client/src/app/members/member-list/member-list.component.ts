9e52615d9414cd1748fea7fbf57b18c5c1b63cfc
Gharbi <leithgharbi@yahoo.fr>
2022-04-29 15:24:49 +0200

    Using the service to store state

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/_modules/member';
 import { MembersService } from 'src/app/_services/members.service';
 
@@ -8,16 +9,17 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members: Member[];
+  members$: Observable<Member[]>;
   constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
-    this.loadMembers();
+    this.members$ = this.memberService.getMembers();
+    // this.loadMembers();
     //console.log('members = ', this.members);
   }
-  loadMembers() {
-    this.memberService.getMembers().subscribe((response) => {
-      this.members = response;
-    });
-  }
+  // loadMembers() {
+  //   this.memberService.getMembers().subscribe((response) => {
+  //     this.members = response;
+  //   });
+  // }
 }

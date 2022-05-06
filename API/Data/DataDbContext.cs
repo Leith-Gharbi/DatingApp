81eb5ecd9001fb0ab5e5df2ed9fd8ea039eb4285
Gharbi <leithgharbi@yahoo.fr>
2022-05-07 00:46:50 +0200

    Adding a likes entity

--- API/Data/DataDbContext.cs
+++ API/Data/DataDbContext.cs
@@ -9,11 +9,38 @@ namespace API.Data
 {
     public class DataDbContext : DbContext
     {
-        public DataDbContext( DbContextOptions options) : base(options)
+        public DataDbContext(DbContextOptions options) : base(options)
         {
 
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+            // For composed key 
+            builder.Entity<UserLike>().HasKey(k => new { k.SourceUserId, k.LikedUserId });
+
+
+            // configure the relation ships
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
     }
 }
--- API/Data/Migrations/20220506224328_likeentityAdded.Designer.cs
+++ API/Data/Migrations/20220506224328_likeentityAdded.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace API.Data.Migrations
 {
     [DbContext(typeof(DataDbContext))]
-    partial class DataDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220506224328_likeentityAdded")]
+    partial class likeentityAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

c572204375482994958a10547540af347cda5284
Gharbi <leithgharbi@yahoo.fr>
2022-05-07 01:16:16 +0200

    Adding a likes repository

---
+++ API/Data/LikesRepository.cs
@@ -0,0 +1,35 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class LikesRepository : ILikesRepository
+    {
+        private readonly DataDbContext _context;
+
+        public LikesRepository(DataDbContext context, IMapper mapper)
+        {
+            _context = context;
+        }
+        public Task<UserLike> GetUserLike(int sourceUserId, int likeUserId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AppUser> GetUserWithLikes(int UserId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,18 @@
+﻿using API.DTOs;
+using API.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+  public  interface ILikesRepository
+    {
+        Task<UserLike> GetUserLike(int sourceUserId, int likeUserId);
+        Task<AppUser> GetUserWithLikes(int UserId);
+        Task<IEnumerable<LikeDto>> GetUserLikes(string predicate , int userId);
+        
+
+    }
+}

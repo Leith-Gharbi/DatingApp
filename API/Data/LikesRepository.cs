d50a88418fc7015a670cf7a4869369742b9f71a3
Gharbi <leithgharbi@yahoo.fr>
2022-05-07 11:01:41 +0200

    Adding pagination for the like

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -1,6 +1,7 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
@@ -24,24 +25,24 @@ namespace API.Data
             return await _context.Likes.FindAsync(sourceUserId, likeUserId);
         }
 
-        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
         {
 
             var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
             var likes = _context.Likes.AsQueryable();
 
-            if(predicate =="liked")
+            if(likesParams.Predicate == "liked")
             {
-                likes = likes.Where(like => like.SourceUserId == userId);
+                likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
                 users = likes.Select(like => like.LikedUser);
             }
-             if(predicate == "likedBy")
+             if(likesParams.Predicate == "likedBy")
             {
-                likes = likes.Where(like => like.LikedUserId == userId);
+                likes = likes.Where(like => like.LikedUserId == likesParams.UserId);
                 users = likes.Select(like => like.SourceUser);
             }
 
-            return await users.Select(user => new LikeDto
+            var likedUsers = users.Select(user => new LikeDto
             {
                 Username =user.UserName,
                 KnownAs = user.KnownAs,
@@ -50,7 +51,9 @@ namespace API.Data
                 PhotoUrl = user.Photos.FirstOrDefault(p=>p.IsMain).Url,
                 Id= user.Id,
 
-            }).ToListAsync();
+            });
+
+            return await PagedList<LikeDto>.CreateAsync(likedUsers, likesParams.PageNumber, likesParams.PageSize);
 
 
         }

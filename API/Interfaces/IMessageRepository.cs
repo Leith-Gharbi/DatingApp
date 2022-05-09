1e4db95e59d59123478b222ed8eb0e8d42db8887
Gharbi <leithgharbi@yahoo.fr>
2022-05-09 12:28:09 +0200

    Setting up the message repository

---
+++ API/Interfaces/IMessageRepository.cs
@@ -0,0 +1,22 @@
+﻿using API.DTOs;
+using API.Entities;
+using API.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+   public interface IMessageRepository
+    {
+
+        void AddMessage(Message message);
+        void DeletMessage(Message message);
+
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+    }
+}

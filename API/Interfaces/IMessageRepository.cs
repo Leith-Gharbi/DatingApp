27f1e53d194a77041f81eea4ec7633f66cc6d0a4
Gharbi <leithgharbi@yahoo.fr>
2022-05-09 15:29:36 +0200

    Getting the message thread for 2 users

--- API/Interfaces/IMessageRepository.cs
+++ API/Interfaces/IMessageRepository.cs
@@ -16,7 +16,7 @@ namespace API.Interfaces
 
         Task<Message> GetMessage(int id);
         Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams massageParams);
-        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername, string recipientUsername);
         Task<bool> SaveAllAsync();
     }
 }

dd9e46c7bc720dc0717cb72678ad34e3552e7c73
Gharbi <leithgharbi@yahoo.fr>
2022-05-14 11:30:46 +0200

    Adding an admin guard

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -24,6 +24,9 @@ export class AccountService {
     );
   }
   setCurrentUser(user: User) {
+    user.roles = [];
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? (user.roles = roles) : user.roles.push(roles);
     localStorage.setItem('user', JSON.stringify(user));
 
     this.currentUserSource.next(user);
@@ -38,10 +41,13 @@ export class AccountService {
       map((user: User) => {
         if (user) {
           this.setCurrentUser(user);
-
         }
         return user;
       })
     );
+  }
+
+  getDecodedToken(token) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 }
